387f3c49441297fe0532ab9137f76c1da000d8fc
ashbelmosomi-wq <ashbelmosomi@gmail.com>
2026-06-14 15:43:25 +0300

    Add files via upload

---
+++ mobile_sales_receipt.cpp
@@ -0,0 +1,37 @@
+//#include <iostream>
+//#include <iomanip>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//    // Variables to store customer details
+//    string customerName, phoneModel;
+//    int quantity;
+//    double pricePerPhone, totalSales;
+//
+//    // Input
+//    cout << "Enter customer name: ";
+//    getline(cin, customerName);
+//    cout << "Enter phone model purchased: ";
+//    getline(cin, phoneModel);
+//    cout << "Enter quantity bought: ";
+//    cin >> quantity;
+//    cout << "Enter price per phone: ";
+//    cin >> pricePerPhone;
+//
+//    // Calculation
+//    totalSales = quantity * pricePerPhone;
+//
+//    // Display receipt
+//    cout << "\n========== SALES RECEIPT ==========\n";
+//    cout << "Customer Name    : " << customerName << endl;
+//    cout << "Phone Model      : " << phoneModel << endl;
+//    cout << "Quantity         : " << quantity << endl;
+//    cout << "Price per Phone  : " << fixed << setprecision(2) << pricePerPhone << endl;
+//    cout << "Total Sales      : " << totalSales << endl;
+//    cout << "===================================\n";
+//
+//    return 0;
+//}
+// Created by Administrator on 14/06/2026.
+//
